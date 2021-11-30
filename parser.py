c2868b7299639f8367fc1d38e21d8938cc577d05
kyrychenko <m.kyrychenko@ukr.net>
2021-11-30 20:04:09 +0200

    3 commit.

--- parser.py
+++ parser.py
@@ -1,1 +1,16 @@
-print('dsgvsadff')ghhdrhtrhddrt
+from urllib import parse
+
+def parse(url):
+
+    parse.urlsplit(url)
+    parse.parse_qs(parse.urlsplit(url).query)
+    a = dict(parse.parse_qsl(parse.urlsplit(url).query))
+    return {a}
+#f __name__ == '__main__':
+    #Testing function `parse`
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/') == {}
+    assert parse('https://example.com/?') == {}
+    assert parse('https://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'feirret', 'color': 'purple'}
